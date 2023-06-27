f146c39f44eaea585afa0173b83a165f4530ff67
comesanha <raphael.comesanha@gmail.com>
2023-06-27 10:25:18 -0300

    Estrutura básica makefile: complinado bibliotecas

---
+++ basic_structure/my_lib.hpp
@@ -0,0 +1,6 @@
+// #pragma once can reduce build times, as the compiler won't open and read the file again 
+// after the first #include of the file in the translation unit.
+#pragma once 
+
+
+void print_msg();
